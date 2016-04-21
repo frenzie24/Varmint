fdbce99ccdf6e65b97f6989a067f48a576ec2bb8
Kevin <kevin.gross@tk-squared.com>
2016-04-21 19:03:24 -0400

    Share intent fixes.

--- app/src/main/java/com/tk_squared/varmint/TkkActivity.java
+++ app/src/main/java/com/tk_squared/varmint/TkkActivity.java
@@ -125,11 +125,11 @@ public class TkkActivity extends AppCompatActivity
             MenuItem item = menu.findItem(R.id.menu_item_share);
             ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
-            shareIntent.setType("image/*");
+            shareIntent.setType("text/plain");
             shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(getString(R.string.app_icon_url)));
             shareIntent.putExtra(Intent.EXTRA_TITLE, getString(R.string.app_name));
-            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message)
-                    + getString(R.string.subtitle) + '\n' + ((TkkWebViewFragment) fragment).getCurrentName() +
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message) + " "
+                    + getString(R.string.subtitle) + '\n' + fragment.getArguments().getString("name") +
                     '\n' + getString(R.string.get_message) + '\n' + getString(R.string.download_url));
             mShareActionProvider.setShareIntent(shareIntent);
         }
